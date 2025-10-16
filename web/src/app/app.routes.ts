c4f8916694bfab893f59bca15610e16e40d27267
Jobin <jobinjose.in@icloud.com>
2025-10-17 04:24:56 +0530

    Initial Dashboard screen

---
+++ web/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { NavbarComponent } from './features/navbar/navbar.component';
+
+export const routes: Routes = [
+    {path: '', component: DashboardComponent}
+];
